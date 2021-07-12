19c1d6722f0318ccd9858ef275bd9ca012ef2acc
Ahmad Catur Yulianto <66737336+CaturSkak@users.noreply.github.com>
2021-07-12 09:01:54 +0700

    unggah pemograman web Fotocopytio
    dasar

--- application/views/admin/konten/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Hello World</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia laudantium ut dolorum quasi. Sint aut suscipit officia nobis, odit est harum expedita, natus molestiae voluptas, sunt dicta! Quis, excepturi commodi!</p>
