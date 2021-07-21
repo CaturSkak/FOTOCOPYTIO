0642474b5e19d57c12ca44a5e4f2c110c6f0fcc7
FarhanJulystio <farhanjt70@gmail.com>
2021-07-21 10:55:07 +0700

    12

---
+++ application/views/frontend/index.php
@@ -0,0 +1,43 @@
+	<div class="owl-carousel owl-loaded owl-nav-dots-inner" data-options='{"items":1,"loop":true}'>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/logo.png); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/a2.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/a4.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/a3.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
